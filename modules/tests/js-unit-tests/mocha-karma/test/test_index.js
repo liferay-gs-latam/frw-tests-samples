e181fe7a5cf2cbf4ebaf7884be0bd377613b74dd
Diego Nascimento <diego.nascimento@liferay.com>
2018-01-24 14:37:43 -0300

    Removing unecessary test_index.js

--- modules/tests/js-unit-tests/mocha-karma/test/test_index.js
+++
@@ -1,7 +0,0 @@
-const tests = require.context('./', true, /index\.js$/);
-
-tests.keys().forEach(tests);
-
-const sources = require.context('../src/', true, /index\.js$/);
-
-sources.keys().forEach(sources);
